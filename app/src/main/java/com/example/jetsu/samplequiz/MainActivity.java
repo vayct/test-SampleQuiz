1e1477db321d773a1813ece474fd8d4d8b2b3da9
vayct <vayct@slu.edu>
2017-05-25 11:38:53 -0600

    Reformat codes

--- app/src/main/java/com/example/jetsu/samplequiz/MainActivity.java
+++ app/src/main/java/com/example/jetsu/samplequiz/MainActivity.java
@@ -1,8 +1,7 @@
 package com.example.jetsu.samplequiz;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -12,39 +11,32 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void submitAnswer(View view){
+    public void submitAnswer(View view) {
 
         int totalPoint = getQuestion1Answer() + getQuestion2Answer() + getQuestion3Answer() + getQuestion4Answer();
         TextView answer = (TextView) findViewById(R.id.answer_text);
 
-        if(totalPoint == 8)
+        if (totalPoint == 8)
             answer.setText("Congratulation, you have answered all of the questions correctly!"
-                            + "\nTotal Points: " +totalPoint + " / 8");
+                    + "\nTotal Points: " + totalPoint + " / 8");
 
         else
             answer.setText("You did not answer all of the questions correctly."
-                            + "\nTotal Points: " + totalPoint + " / 8");
-
-
+                    + "\nTotal Points: " + totalPoint + " / 8");
 
 
     }
 
 
-
-
-    private int getQuestion1Answer()
-    {
+    private int getQuestion1Answer() {
         RadioButton q1Answer = (RadioButton) findViewById(R.id.european_or_african);
-        if(q1Answer.isChecked())
+        if (q1Answer.isChecked())
             return 1;
         else
             return 0;
@@ -52,47 +44,42 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
-
-    private int getQuestion3Answer()
-    {
+    private int getQuestion3Answer() {
 
         int question3Point = 0;
 
         CheckBox c1 = (CheckBox) findViewById(R.id.text_view);
-        if(c1.isChecked())
+        if (c1.isChecked())
             question3Point++;
 
 
         CheckBox c3 = (CheckBox) findViewById(R.id.button);
-        if(c3.isChecked())
+        if (c3.isChecked())
             question3Point++;
 
         CheckBox c4 = (CheckBox) findViewById(R.id.scroll_view);
-        if(c4.isChecked())
+        if (c4.isChecked())
             question3Point++;
-
 
 
         CheckBox c6 = (CheckBox) findViewById(R.id.check_box);
-        if(c6.isChecked())
+        if (c6.isChecked())
             question3Point++;
 
 
-
         CheckBox c9 = (CheckBox) findViewById(R.id.edit_text);
-        if(c9.isChecked())
+        if (c9.isChecked())
             question3Point++;
 
         return question3Point;
     }
 
-    private int getQuestion2Answer(){
+    private int getQuestion2Answer() {
 
-        EditText q2Answer =  (EditText) findViewById(R.id.q2_answer);
-        String q2AnswerString =  q2Answer.getText().toString();
+        EditText q2Answer = (EditText) findViewById(R.id.q2_answer);
+        String q2AnswerString = q2Answer.getText().toString();
 
-        if(q2AnswerString.compareTo(getString(R.string.question2_answer)) == 0)
+        if (q2AnswerString.compareTo(getString(R.string.question2_answer)) == 0)
             return 1;
         else
             return 0;
@@ -101,13 +88,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private int getQuestion4Answer(){
+    private int getQuestion4Answer() {
 
         EditText q4Answer = (EditText) findViewById(R.id.q4_answer);
         String q4AnswerString = q4Answer.getText().toString();
 
-        if(q4AnswerString.compareTo(getString(R.string.question4_answer)) == 0)
-            return  1;
+        if (q4AnswerString.compareTo(getString(R.string.question4_answer)) == 0)
+            return 1;
         else
             return 0;
 
